fd744dccbf95f39a010b9ddd7b9e2d7687853cdd
nicorivas <nicolas@klog.co>
2023-09-04 16:22:34 -0300

    User profile

--- app/settings.py
+++ app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "company.apps.CompanyConfig",
     "instruction.apps.InstructionConfig",
     "chat.apps.ChatConfig",
+    "user.apps.UserConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
